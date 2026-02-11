404e5f605140f6a12e62818021f8435dadcf7e3f
Lina <lina@tristagram.pictures>
2026-02-11 16:06:50 +0100

    Initial commit

---
+++ app/src/main/java/pictures/tristagram/lina/bodytune/MainActivity.java
@@ -0,0 +1,68 @@
+package pictures.tristagram.lina.bodytune;
+
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+//        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        WebView webView = new WebView(this);
+        // В Activity:
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        setContentView(webView);
+
+
+        // 2. Сдвигаем контент вниз на высоту системной панели
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(0, insets.top, 0, 0);
+            return windowInsets;
+        });
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true); // Для сохранения веса в памяти
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        // Поддержка темной темы на уровне WebView
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            settings.setForceDark(WebSettings.FORCE_DARK_AUTO);
+        }
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            // Этот метод открывает системный выбор файла
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                // Здесь нужен код для открытия Intent.ACTION_GET_CONTENT
+                // Для простоты: импорт лучше сделать через текстовое поле (вставить текст из CSV)
+                // или использовать полноценный FileChooser.
+                return true;
+            }
+        });
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
